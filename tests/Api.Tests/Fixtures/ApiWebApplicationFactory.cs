086495ebe6bc31073a3b63b4ec2e205967fde3e0
Danylo <daniilklimenko@meta.ua>
2023-10-19 18:08:19 +0300

    tests: postgres has dynamic api, for potential tests parallel run.

--- tests/Api.Tests/Fixtures/ApiWebApplicationFactory.cs
+++ tests/Api.Tests/Fixtures/ApiWebApplicationFactory.cs
@@ -1,11 +1,9 @@
 using App.Data;
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Containers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
 using Xunit;
 
 namespace Api.Tests.Fixtures;
@@ -14,22 +12,23 @@ namespace Api.Tests.Fixtures;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class ApiWebApplicationFactory : WebApplicationFactory<TestProgram>, IAsyncLifetime
 {
-    private readonly IContainer _postgres = new ContainerBuilder()
-        .WithImage("postgres:latest")
-        .WithEnvironment("POSTGRES_USER", "postgres")
-        .WithEnvironment("POSTGRES_PASSWORD", "mysecretpassword")
-        .WithEnvironment("POSTGRES_DB", "the_app")
-        .WithPortBinding(5555, 5432)
-        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+        .WithUsername("postgres")
+        .WithPassword("mysecretpassword")
+        .WithDatabase("the_app")
         .Build();
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         var configuration = new ConfigurationBuilder()
             .AddJsonFile("testsettings.json")
+            .AddInMemoryCollection(new Dictionary<string, string?>()
+            {
+                { "ConnectionStrings:Postgres", _postgres.GetConnectionString() }
+            })
             .Build();
         builder.UseConfiguration(configuration);
     }
-    
+
     public IServiceScope CreateScope()
     {
         return Services.CreateScope();
