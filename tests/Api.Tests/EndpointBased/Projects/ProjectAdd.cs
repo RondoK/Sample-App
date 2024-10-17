adce0d9148c65d40e5f7495c2f0397c32f6b874a
Danylo <daniilklimenko@meta.ua>
2024-10-18 00:00:02 +0300

    refac: tests. ProjectAdd ordered tests

--- tests/Api.Tests/EndpointBased/Projects/ProjectAdd.cs
+++ tests/Api.Tests/EndpointBased/Projects/ProjectAdd.cs
@@ -2,10 +2,11 @@ using Api.Tests.Fixtures;
 using App.Data.Models;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Priority;
 
 namespace Api.Tests.EndpointBased.Projects;
 
-//TODO : think about how to Run once check multiple asserts, and if it is needed
+[TestCaseOrderer(PriorityOrderer.Name, PriorityOrderer.Assembly)]
 public class ProjectAdd : ResetDbFixture, IClassFixture<ClientFixture>
 {
     private readonly ClientFixture _fixture;
@@ -21,20 +22,19 @@ public class ProjectAdd : ResetDbFixture, IClassFixture<ClientFixture>
     }
 
     //TODO: Try Add AAA styled Code generation  or unwrapping function tool 
-    [Fact]
-    
+    [Fact, Priority(1)]
     public async Task ReturnsSameObjWithNewId() =>
         await CustomAsserts.AddNew_ReturnsSameObjWithNewId<Project, int>(_fromClient, _server);
 
-    [Fact]
+    [Fact, Priority(2)]
     public async Task CanBeRetrievedById() =>
         await CustomAsserts.AddNew_CanBeRetrievedById<Project, int>(_fromClient, _server);
     
-    [Fact]
+    [Fact, Priority(3)]
     public async Task CanBerRetrievedInPages() =>
         await CustomAsserts.AddNew_CanBeRetrievedInPaged(_fromClient, _server, 10);
 
-    [Fact]
+    [Fact, Priority(4)]
     public async Task CanBeRetrievedInList() =>
         await CustomAsserts.AddNew_CanBeRetrievedInList(_fromClient, _server);
 }
